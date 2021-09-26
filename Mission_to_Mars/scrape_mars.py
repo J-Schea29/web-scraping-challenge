f09e6dcdf8e0e985f6272ddf0fe28f567e10b26b
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-09-26 10:11:22 -0700

    Update scrape_mars.py

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,1 +1,76 @@
-def scrape
+from bs4 import BeautifulSoup as bs
+import requests
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+def scrape():
+    
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    
+    #Mars News
+    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    news_title = soup.find_all('div', class_='content_title')[1].text
+    news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+    
+    #Feature Image
+    image_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(image_url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    featured = soup.find('img', class_='headerimage fade-in').get('src')
+    feature_image = f"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{featured}"
+    
+    #Mars Tables
+    facts_url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(requests.get(facts_url).text)
+    mars_df = tables[0]
+    mars_df.columns=["Description", "Value"]
+    html_table = mars_df.to_html()
+    
+    #hemisphere_images
+    images_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(images_url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    hemisphere_image_urls = []
+    hem_pics = soup.find_all('div', class_='item')
+    
+    for pic in hem_pics:
+        title = pic.find('h3').text
+        partial = pic.find('a', clas_='itemLink product-item')['href']
+        print(title)
+        print(partial)
+        browser.visit(f"https://astrogeology.usgs.gov{partial}")
+        partial_html = browser.html
+        soup = bs(partial_html, 'html.parser')
+        img = soup.find('a', clas_='itemLink product-item')['href']
+        img_url = f"https://astrogeology.usgs.gov{img}"
+        hemisphere_image_urls.append({"title" : title, "img_url" : img_url})
+    
+    mars_dict = {"news_title": news_title,
+                 "news_paragraph": news_p,
+                 "feature_image": feature_image,
+                 "mars_table": html_table,
+                 "hemispere_images": hemisphere_image_urls
+                }
+    return mars_dict
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
